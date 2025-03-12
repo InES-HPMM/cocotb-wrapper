f6eb67ee1ab65de5f164676dba388675c40aecd2
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-03-12 20:08:29 +0100

    feat: format nix files with alejandra

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.1.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,62 +28,63 @@
     };
   };
 
-  outputs = { self, nixpkgs, uv2nix, pyproject-nix, pyproject-build-systems, }:
-    let
-      inherit (nixpkgs) lib;
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      formatter = forAllSystems (system: pkgs.${system}.nixpkgs-fmt);
-      packages = forAllSystems (system:
-        let
-          workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-          overlay = workspace.mkPyprojectOverlay {
-            sourcePreference = "wheel"; # or sourcePreference = "sdist";
+  outputs = {
+    self,
+    nixpkgs,
+    alejandra,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
+  }: let
+    inherit (nixpkgs) lib;
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+  in {
+    formatter = forAllSystems (system: alejandra.defaultPackage.${system});
+    packages = forAllSystems (system: let
+      workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel"; # or sourcePreference = "sdist";
+      };
+      pythonSet =
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit (pkgs.${system}) python3;
+        })
+        .overrideScope
+        (
+          lib.composeManyExtensions [
+            pyproject-build-systems.overlays.default
+            overlay
+          ]
+        );
+    in {
+      default = pythonSet.mkVirtualEnv "cocotb-wrapper" workspace.deps.default;
+    });
+    devShells = forAllSystems (system: {
+      default = pkgs.${system}.mkShell {
+        name = "cocotb-wrapper";
+        packages = with pkgs.${system}; [
+          python3
+          python3Packages.uv
+          pre-commit
+        ];
+        env =
+          {
+            # Prevent uv from managing Python downloads
+            UV_PYTHON_DOWNLOADS = "never";
+            # Force uv to use nixpkgs Python interpreter
+            UV_PYTHON = pkgs.${system}.python3.interpreter;
+          }
+          // lib.optionalAttrs pkgs.${system}.stdenv.isLinux {
+            # Python libraries often load native shared objects using dlopen(3).
+            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
           };
-          pythonSet =
-            (pkgs.callPackage pyproject-nix.build.packages {
-              inherit (pkgs.${system}) python3;
-            }).overrideScope
-              (
-                lib.composeManyExtensions [
-                  pyproject-build-systems.overlays.default
-                  overlay
-                ]
-              );
-        in
-        {
-          default = pythonSet.mkVirtualEnv "cocotb-wrapper" workspace.deps.default;
-        });
-      devShells = forAllSystems (system:
-        let
-        in
-        {
-          default = pkgs.${system}.mkShell {
-            name = "cocotb-wrapper";
-            packages = with pkgs.${system}; [
-              python3
-              python3Packages.uv
-              pre-commit
-            ];
-            env =
-              {
-                # Prevent uv from managing Python downloads
-                UV_PYTHON_DOWNLOADS = "never";
-                # Force uv to use nixpkgs Python interpreter
-                UV_PYTHON = pkgs.${system}.python3.interpreter;
-              }
-              // lib.optionalAttrs pkgs.${system}.stdenv.isLinux {
-                # Python libraries often load native shared objects using dlopen(3).
-                # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-                LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-              };
-            shellHook = ''
-              unset PYTHONPATH
-            '';
-          };
-        });
-    };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
+      };
+    });
+  };
 }
