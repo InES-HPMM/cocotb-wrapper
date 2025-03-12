45939230673dc45cfb07b3e112aa947973595027
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-03-12 21:17:15 +0100

    fix: cleanup flake

--- flake.nix
+++ flake.nix
@@ -29,12 +29,12 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     alejandra,
     uv2nix,
     pyproject-nix,
     pyproject-build-systems,
+    ...
   }: let
     inherit (nixpkgs) lib;
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
@@ -45,7 +45,7 @@
     packages = forAllSystems (system: let
       workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
       overlay = workspace.mkPyprojectOverlay {
-        sourcePreference = "wheel"; # or sourcePreference = "sdist";
+        sourcePreference = "wheel";
       };
       pythonSet =
         (pkgs.callPackage pyproject-nix.build.packages {
@@ -71,14 +71,10 @@
         ];
         env =
           {
-            # Prevent uv from managing Python downloads
             UV_PYTHON_DOWNLOADS = "never";
-            # Force uv to use nixpkgs Python interpreter
             UV_PYTHON = pkgs.${system}.python3.interpreter;
           }
           // lib.optionalAttrs pkgs.${system}.stdenv.isLinux {
-            # Python libraries often load native shared objects using dlopen(3).
-            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
             LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
           };
         shellHook = ''
