391ad968ef1a41a336cf223c89b0a6cedace72f2
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-04-11 09:24:51 +0200

    chore: add `default.nix` and `shell.nix` to support development without nix flakes

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
     flake-utils.url = "github:numtide/flake-utils";
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
